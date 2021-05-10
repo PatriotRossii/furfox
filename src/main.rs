b07e600a345f8162234b9509a0e9e47dbc3f2702
PatriotRossii <patriotrossii2019@mail.ru>
2021-05-10 18:04:05 +0500

    Rewrite input to crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
-use std::io::{self, Read};
+use crossterm::{ErrorKind, event::{Event, KeyCode, KeyModifiers}, terminal::enable_raw_mode};
+use crossterm::event::read;
 
-use crossterm::terminal::enable_raw_mode;
-
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-fn die(e: std::io::Error) {
+fn die(e: ErrorKind) {
     eprintln!("error: {:?}", e);
     std::process::exit(1);
 }
 
 fn main() {
     enable_raw_mode().unwrap();
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
+    
+    loop {
+        match read() {
+            Ok(event) => {
+                match event {
+                    Event::Key(e) => {
+                        if let KeyCode::Char(c) = e.code {
+                            if c.is_control() {
+                                println!("{:?}\r", c as u8);
+                            } else {
+                                println!("{:?} ({})\r", c as u8, c);
+                            }
+        
+                            if c == 'q' && e.modifiers == KeyModifiers::CONTROL {
+                                break
+                            }
+                        } else {
+                            println!("{:?}\r", e.code);
+                        }            
+                    },
+                    _ => {}
+                }        
             }
-            Err(err) => die(err)
+            Err(e) => {
+                die(e)
+            }
         }
     }
 }
