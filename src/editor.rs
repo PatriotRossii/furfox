626ebf8530de9f943bbc27a8d4ca00bea6a39a1a
PatriotRossii <patriotrossii2019@mail.ru>
2021-05-10 18:17:30 +0500

    Fix formatting

--- src/editor.rs
+++ src/editor.rs
@@ -1,39 +1,39 @@
-use crossterm::{ErrorKind, event::{Event, KeyCode, KeyModifiers}, terminal::{disable_raw_mode, enable_raw_mode}};
 use crossterm::event::read;
+use crossterm::{
+    event::{Event, KeyCode, KeyModifiers},
+    terminal::{disable_raw_mode, enable_raw_mode},
+    ErrorKind,
+};
 
-pub struct Editor { }
+pub struct Editor {}
 
 impl Editor {
     pub fn run(&self) {
         if let Err(e) = enable_raw_mode() {
             die(e);
         }
-        
+
         loop {
             match read() {
-                Ok(event) => {
-                    match event {
-                        Event::Key(e) => {
-                            if let KeyCode::Char(c) = e.code {
-                                if c.is_control() {
-                                    println!("{:?}\r", c as u8);
-                                } else {
-                                    println!("{:?} ({})\r", c as u8, c);
-                                }
-            
-                                if c == 'q' && e.modifiers == KeyModifiers::CONTROL {
-                                    break
-                                }
+                Ok(event) => match event {
+                    Event::Key(e) => {
+                        if let KeyCode::Char(c) = e.code {
+                            if c.is_control() {
+                                println!("{:?}\r", c as u8);
                             } else {
-                                println!("{:?}\r", e.code);
-                            }            
-                        },
-                        _ => {}
-                    }        
-                }
-                Err(e) => {
-                    die(e)
-                }
+                                println!("{:?} ({})\r", c as u8, c);
+                            }
+
+                            if c == 'q' && e.modifiers == KeyModifiers::CONTROL {
+                                break;
+                            }
+                        } else {
+                            println!("{:?}\r", e.code);
+                        }
+                    }
+                    _ => {}
+                },
+                Err(e) => die(e),
             }
         }
 
@@ -45,7 +45,7 @@ impl Editor {
 
 impl Default for Editor {
     fn default() -> Self {
-        Self { }
+        Self {}
     }
 }
 
